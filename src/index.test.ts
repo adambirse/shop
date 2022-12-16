31a30c0e41231f608808028b4d53bb8da5ddeb93
Adam Birse <adambirse@gmail.com>
2022-12-16 12:54:24 +0000

    prettier added

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
-import { sum } from "./";
+import { sum } from './';
 
-describe("sum function", () => {
-  it("adds 2 numbers", () => {
+describe('sum function', () => {
+  it('adds 2 numbers', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
