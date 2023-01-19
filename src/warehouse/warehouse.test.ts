33868f379fc5d49e62619ba7ca8868ce2ee5c2b7
Adam Birse <adambirse@gmail.com>
2023-01-19 15:09:00 +0000

    add warehouse capacity

--- src/warehouse/warehouse.test.ts
+++ src/warehouse/warehouse.test.ts
@@ -4,6 +4,14 @@ describe('Warehouse', () => {
   it('Can create a warehouse', () => {
     expect(new Warehouse(products)).toBeDefined();
   });
+
+  it('Can create a warehouse with optional capacity', () => {
+    expect(new Warehouse(products, 5)).toBeDefined();
+  });
+
+  it('Can not create a warehouse if capacity is exceeded', () => {
+    expect(() => new Warehouse(products, 1)).toThrow(Error);
+  });
   it('Can get products from warehouse', () => {
     const warehouse = new Warehouse(products);
     expect(warehouse.getProducts()).toBe(products);
@@ -12,7 +20,13 @@ describe('Warehouse', () => {
   it('Can add products', () => {
     const warehouse = new Warehouse(products);
     expect(warehouse.getProducts().length).toBe(2);
-    warehouse.add('new producsts');
+    warehouse.add('new products');
     expect(warehouse.getProducts().length).toBe(3);
+  });
+
+  it('Cannot add products if capacity exceeded', () => {
+    const warehouse = new Warehouse(products, 2);
+    expect(() => warehouse.add('new products')).toThrow(Error);
+    expect(warehouse.getProducts().length).toBe(2);
   });
 });
--- src/warehouse/warehouse.ts
+++ src/warehouse/warehouse.ts
@@ -1,14 +1,28 @@
 export class Warehouse {
+  DEFAULT_CAPACITY = 10;
   products: string[];
+  capacity: number;
 
-  constructor(products: string[]) {
-    this.products = products;
+  constructor(products: string[], capacity?: number) {
+    this.capacity = capacity || this.DEFAULT_CAPACITY;
+    if (products.length <= this.capacity) {
+      this.products = products;
+    } else {
+      throw new Error('Capacity exceeded');
+    }
   }
 
   getProducts(): string[] {
     return this.products;
   }
   add(product: string) {
-    this.products = this.products.concat(product);
+    if (this.hasCapacity()) {
+      this.products = this.products.concat(product);
+    } else {
+      throw new Error('Warehouse full');
+    }
+  }
+  private hasCapacity() {
+    return this.products.length < this.capacity;
   }
 }
