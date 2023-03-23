947cfe351163c01b99b2789c9cf87ece57d786de
Adam Birse <adambirse@gmail.com>
2023-03-23 13:22:54 +0000

    Comments explaining operations and repositories

--- src/domain/operations.ts
+++ src/domain/operations.ts
@@ -1,6 +1,10 @@
 import { Product } from './model/product/product';
 import { Warehouse } from './model/warehouse/warehouse';
 
+// An interface describing the domain operations this business domain supports.
+// These should be as close to the domain language used by the SMEs as possible
+// as they represent concepts the business defines, rather than developers.
+// They will be implemented by sone or more Services
 export interface Operations {
   createWarehouse(capacity: number): Warehouse;
   getWarehouse(id: string): Warehouse | undefined;
--- src/domain/repository/warehouseRepository.ts
+++ src/domain/repository/warehouseRepository.ts
@@ -1,5 +1,11 @@
 import { Warehouse } from '../model/warehouse/warehouse';
-
+// Repositories should be defined as an interface, so that they
+// can support implementation as different ORM or test instances.
+//
+// The repository MUST NOT implement business logic, such as making
+// decisions on the validity of the given operation, or mutating the
+// content of the object.  The repository is solely concerned with
+// moving the data into and out of the persistence engine correctly.
 export interface WarehouseRepository {
   save(warehouse: Warehouse): Warehouse;
   get(id: string): Warehouse | undefined;
