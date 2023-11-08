7ecc49173191ae9c522ea2a66f1a6e2d53115016
Adam Birse <adambirse@gmail.com>
2023-11-08 18:35:59 +0000

    warehouse mapper test

--- src/adaptor/typeORMRepository/mappers/warehousedaoMapper.test.ts
+++ src/adaptor/typeORMRepository/mappers/warehousedaoMapper.test.ts
@@ -2,10 +2,16 @@ import { Warehouse } from '../../../domain/model/warehouse/warehouse';
 import { mapToDao } from './warehousedaoMapper';
 
 describe('warehouse mapper', () => {
-  
   const warehouse: Warehouse = new Warehouse([]);
 
-  it('should map to Dao', () => {
-    expect(mapToDao(warehouse)).toBeDefined();
+  it('should map to Dao without any products', () => {
+    const warehouseDao = mapToDao(warehouse);
+    expect(warehouseDao.domainId).toEqual(warehouse.id);
+    expect(warehouseDao.capacity).toEqual(warehouse.capacity);
+    expect(warehouseDao.products).toEqual([]);
+  });
+  it('should not map database id', () => {
+    const warehouseDao = mapToDao(warehouse);
+    expect(warehouseDao.id).toBeUndefined;
   });
 });
